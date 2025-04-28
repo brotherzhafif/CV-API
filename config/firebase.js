9321194772b93fcb634705062bd229f49b97a8c5
brotherzhafif <bangz1504@gmail.com>
2025-04-28 23:22:22 +0700

    fix: Fixing Deployment Code and Deleting Unused File

--- config/firebase.js
+++ config/firebase.js
@@ -1,16 +1,16 @@
-require('dotenv').config(); // Tambahkan ini di bagian atas
+require('dotenv').config();
 const admin = require('firebase-admin');
 
 admin.initializeApp({
     credential: admin.credential.cert({
         projectId: process.env.FIREBASE_PROJECT_ID,
         clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-        privateKey: process.env.FIREBASE_PRIVATE_KEY,
+        privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n') // Ganti \\n dengan newline
     }),
-    storageBucket: `${process.env.FIREBASE_PROJECT_ID}.appspot.com` // Tambahkan bucket default
+    storageBucket: `${process.env.FIREBASE_PROJECT_ID}.appspot.com`
 });
 
 const db = admin.firestore();
-const bucket = admin.storage().bucket(); // Inisialisasi bucket
+const bucket = admin.storage().bucket();
 
 module.exports = { admin, db, bucket };
